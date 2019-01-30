d3aebfc2f13ffcdea69392b11f7f3e5135b90378
Saurabh Singh <46737026+saurabhsingh-qa@users.noreply.github.com>
2019-01-30 18:24:11 +0530

    Add files via upload

---
+++ java/Server.java
@@ -0,0 +1,23 @@
+public class Server extends Computer{
+    private String RAM;
+    private String ROM;
+    private String Processor;
+    public Server(String RAM, String ROM, String Processor){
+        this.RAM = RAM;
+        this.ROM = ROM;
+        this.Processor = Processor;
+
+    }
+
+    public  print(){
+        System.out.println("RAM =" + RAM);
+        System.out.println("ROM =" + ROM);
+        System.out.println("Processor =" + Processor);
+    }
+
+    public static printDetails(){
+        new Server.print();
+    }
+
+
+}
